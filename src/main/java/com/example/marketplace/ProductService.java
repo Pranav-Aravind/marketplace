b8d8fd1b677287445bd8c3cdc232671ef35c0fd0
Pranav-Aravind <pranavaravind2406@gmail.com>
2026-02-13 10:46:31 +0000

    linked my db to html

--- src/main/java/com/example/marketplace/ProductService.java
+++ src/main/java/com/example/marketplace/ProductService.java
@@ -5,6 +5,7 @@ import com.example.marketplace.repository.ProductRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 public class ProductService {
@@ -15,6 +16,11 @@ public class ProductService {
     @Transactional
     public void addProduct(Product prod) {
         productRepository.save(prod);
+    }
+
+    @Transactional
+    public Product findProduct(@PathVariable int id) {
+        return productRepository.findById(id).get();
     }
 
     @Transactional
--- src/main/java/com/example/marketplace/controller/HomeController.java
+++ src/main/java/com/example/marketplace/controller/HomeController.java
@@ -1,16 +1,39 @@
 package com.example.marketplace.controller;
+import com.example.marketplace.ProductService;
+import com.example.marketplace.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/productdetails")
-    public String prodDetails() {
+    @GetMapping("/{id}")
+    public String home(@PathVariable int id, org.springframework.ui.Model model) {
+
+        Product product = productService.findProduct(id);
+
+        model.addAttribute("product", product);
+
+        return "productdetails";
+    }
+
+    @GetMapping("/productdetails/{id}")
+    public String prodDetails(@PathVariable int id, org.springframework.ui.Model model) {
+
+        Product product = productService.findProduct(id);
+
+        model.addAttribute("product", product);
+
         return "productdetails";
     }
 }
